897bd9cdc38bcb9959d0f6517df7a7d9452c0db5
eddie <fdsa0329@naver.com>
2019-07-30 01:57:24 +0900

    Updates

---
+++ precache-manifest.6da6c2c1384b8bfb50fff96c0f64cb1d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7507e52e675fab48af86ed77ce6e012f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9404edeb7cbb4e3b3afb",
+    "url": "/static/css/2.cb759328.chunk.css"
+  },
+  {
+    "revision": "cf17673111e52bc87237",
+    "url": "/static/css/main.6b14fde2.chunk.css"
+  },
+  {
+    "revision": "9404edeb7cbb4e3b3afb",
+    "url": "/static/js/2.5cf5339c.chunk.js"
+  },
+  {
+    "revision": "cf17673111e52bc87237",
+    "url": "/static/js/main.20f8d971.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "30a8b60733b0d96665344e75c0f7f661",
+    "url": "/static/media/Bookshelf.30a8b607.png"
+  },
+  {
+    "revision": "3127cb2627e65171789d2eb2008bbcef",
+    "url": "/static/media/Photo1.3127cb26.jpg"
+  },
+  {
+    "revision": "63dd989079abefe07204e683f9bb2292",
+    "url": "/static/media/Photo2.63dd9890.jpg"
+  },
+  {
+    "revision": "4ef4fa7f32430943d5559b74edffc074",
+    "url": "/static/media/Photo3.4ef4fa7f.jpg"
+  },
+  {
+    "revision": "749befb201c8182dcaee177bd56cf59c",
+    "url": "/static/media/Photo4.749befb2.jpg"
+  },
+  {
+    "revision": "18437b84ce4fd1a4e9dfca51cf58188a",
+    "url": "/static/media/Photo5.18437b84.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Proto__Homepage/precache-manifest.88f3d6bc12b8369e9d9e508f4f91fd0e.js"
+  "/precache-manifest.6da6c2c1384b8bfb50fff96c0f64cb1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Proto__Homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
